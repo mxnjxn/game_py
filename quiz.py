41ef0dd8f4093a486c0b51861f437a32d1ed52b0
mxnjxn <choe86898@gmail.com>
2022-05-23 18:06:41 +0900

    ..

---
+++ quiz.py
@@ -0,0 +1,45 @@
+import pygame
+
+# 기본 초기화
+pygame.init()
+
+# 화면 크기 설정
+screen_width = 640
+screen_height = 480
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# 화면 타이틀 설정
+pygame.display.set_caption("The falling POOP!!")
+
+# FPS (Frame Per Second)
+clock = pygame.time.Clock(30)
+# 1. 사용자 게임 초기화 (배경화면, 게임이미지, 좌표, 속도, 폰트 등)
+
+# 배경 화면 불러오기
+background = pygame.image.load(
+    r"C:\python_workspace\python_application\game_py\Falling_Poop_Game_background.jpg")
+# character
+character = pygame.image.load(
+    r"C:\python_workspace\python_application\game_py\Falling_Poop_Character.png")
+character_width = 70
+character_height = 80
+
+# event loop
+running = True
+while running:
+    dt = clock.tick(30)
+
+    # 2. 이벤트 처리 (키보드, 마우스 등)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # 3. 게임 캐릭터 위치 정의
+
+    # 4. 충돌 처리
+
+    # 5. 화면에 그리기
+    screen.blit(background, (0, 0))
+    pygame.display.update()
+
+pygame.quit()
