efa9e5bc8ac7f7b1d5c9144a3c56c72289086abe
mxnjxn <choe86898@gmail.com>
2022-05-23 15:38:03 +0900

    ....

--- 6_collision.py
+++ 6_collision.py
@@ -37,6 +37,15 @@ to_y = 0
 # 이동속도
 character_speed = 0.6
 
+# enemy 캐릭터
+enemy = pygame.image.load(
+    r"C:\python_workspace\python_application\game_py\enemy.png")
+enemy_size = enemy.get_rect().size  # 이미지의 크기를 구해옴(가로, 세로)
+enemy_width = enemy_size[0]  # character_size의 첫번째 값.
+enemy_height = enemy_size[1]  # character_size의 두번째 값.
+enemy_x_pos = screen_width/2 - enemy_width/2  # 화면 가로의 절반 크기에 해당하는 곳에 위치
+enemy_y_pos = screen_height/2 - enemy_height/2  # 화면 세로 크기 가장 아래에 해당하는 곳에 위치
+
 # event loop --> enables the tabs( event loop이 있어야 창이 꺼지지 않는다.)--> 사용자의 동작을 검사함.
 running = True  # 게임이 진행중인가?
 while running:
@@ -90,10 +99,29 @@ while running:
     elif character_y_pos < 0:
         character_y_pos = 0
 
+    # 충돌 처리를 위한 rect 정보 업데이트
+    # 캐릭터 좌표와 크기 정보 --> 캐릭터 자체 이미지의 rect 정보는 항상 똑같은 곳을 가리키고 있다.
+    character_rect = character.get_rect()
+    character_rect.left = character_x_pos
+    character_rect.top = character_y_pos
+
+    enemy_rect = enemy.get_rect()
+    # 업데이트 안하면 enemy_x,y 에 대한 포지션이 rect에 들어가있지 아니하게 된다. 기본 height, width만 들어가 있다?
+    enemy_rect.left = enemy_x_pos
+    enemy_rect.top = enemy_y_pos
+
+    # 충돌 체크
+    if character_rect.colliderect(enemy_rect):
+        print("충돌 했어요.")
+        running = False
+
     screen.blit(background, (0, 0))  # 배경 그리기 (좌표 설정)
 
     screen.blit(character, (character_x_pos, character_y_pos))  # 캐릭터 그리기
     # screen.fill(0,0,0) --> rgb 값을 넣어서 배경색 그리기
+
+    screen.blit(enemy, (enemy_x_pos, enemy_y_pos))  # 적 그리기
+
     pygame.display.update()  # 개인화면을 다시 그리기 ( pygame 에서는 매 프레임마다 그려줘야 함.)
 # pygame 종료
 pygame.quit()
